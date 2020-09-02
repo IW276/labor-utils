aa61e9cb56fbaf3af7280f5421913696f661e650
Mickael Cormier <mickael.cormier@iosb.fraunhofer.de>
2020-09-02 11:32:27 +0200

    Librealsense udev

--- install_librealsense.sh
+++ install_librealsense.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y libxinerama-dev libxcursor-dev
 wget https://github.com/IntelRealSense/librealsense/archive/v2.38.1.zip
 unzip v2.38.1.zip && rm v2.38.1.zip && mkdir ${HOME}/projects/librealsense-2.38.1/build/
 cd ${HOME}/projects/librealsense-2.38.1/build/ 
-cmake ../ -DFORCE_RSUSB_BACKEND=ON -DBUILD_PYTHON_BINDINGS:bool=true -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_BUILD_TYPE=release -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true -DBUILD_WITH_CUDA:bool=true   
+cmake ../ -DFORCE_LIBUVC=true -DBUILD_PYTHON_BINDINGS:bool=true -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_BUILD_TYPE=release -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true  -DBUILD_WITH_CUDA:bool=true   
 make -j4
 sudo make install
 if ! grep '/usr/local/lib' ${HOME}/.bashrc > /dev/null ; then
@@ -25,3 +25,7 @@ if ! grep '/usr/local/lib' ${HOME}/.bashrc > /dev/null ; then
   echo >> ${HOME}/.bashrc
   echo "PYTHONPATH=$PYTHONPATH:/usr/local/lib" >> ${HOME}/.bashrc
 fi
+
+echo "-- Applying udev rules"
+sudo cp ${HOME}/projects/librealsense-2.38.1/config/99-realsense-libusb.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && udevadm trigger
