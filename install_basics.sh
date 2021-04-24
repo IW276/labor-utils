30ddf643e550736922b887880f07b376a3bbcd73
Mickael Cormier <61741687+MickaelCormier@users.noreply.github.com>
2021-04-24 09:30:04 +0200

    Update install_basics.sh

--- install_basics.sh
+++ install_basics.sh
@@ -13,8 +13,8 @@ sudo -H pip3 install -U  Cython
 sudo -H pip3 install -U numpy
 #Docker
 sudo cp ${HOME}/projects/labor-utils/docker/daemon.json /etc/docker/daemon.json
-# Downdload L4T Pytorch Container 32.4.3
-sudo docker pull nvcr.io/nvidia/l4t-pytorch:r32.4.3-pth1.6-py3
+# Downdload L4T Pytorch Container 32.5.30
+sudo docker pull nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.7-py3
 
 echo "-- Reboot in one minute..."
 sudo shutdown -r 0
